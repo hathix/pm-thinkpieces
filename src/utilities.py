39428f7e152d2b0464428411b72126acfd54ae08
Neel Mehta <neelmehta96@gmail.com>
2020-12-10 09:51:39 -0500

    more

--- src/utilities.py
+++
@@ -1,9 +0,0 @@
-# Utility functions
-
-# Convenience function to safely get an item from a dict.
-# If the key doesn't exist, just returns none
-def safe_get(obj, key):
-    if obj.has_key(key):
-        return obj[key]
-    else:
-        return None
