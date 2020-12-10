a3a89ec9719b9737d6f5eb40d716f17e9831ceed
Neel Mehta <neelmehta96@gmail.com>
2020-12-10 09:59:08 -0500

    new loading code

---
+++ thinkpiecer/feeds.py
@@ -0,0 +1,40 @@
+# Returns a list of all the RSS feeds that we'd like to track.
+def get_feeds():
+    # First, Substacks. Getting their RSS feeds is pretty simple/systematic.
+    substack_domains = [
+        "thegeneralist",
+        "danco",
+        "diff",
+        "nbt",
+        "platformer",
+        "notboring",
+        "sariazout",
+        "digitalnative",
+        "jamesonstartups",
+        "breakingsmart",
+        "artofgig",
+        "theskip",
+        "gwern"
+    ]
+
+    # Feeds are, e.g., https://thegeneralist.substack.com/feed
+    substack_feeds = ["https://{0}.substack.com/feed".format(domain) for domain in substack_domains]
+
+    # Now add some custom RSS feeds
+    # Medium feeds are medium.com/feed/@user or medium.com/feed/publication
+    custom_feeds = [
+        "https://stratechery.com/feed/",
+        "https://www.profgalloway.com/feed",
+        "https://eugene-wei.squarespace.com/blog?format=rss",
+        "https://medium.com/feed/@superwuster",
+        "https://commoncog.com/blog/rss",
+        "https://www.lennyrachitsky.com/feed",
+        "https://medium.com/feed/bloated-mvp",
+        "https://daringfireball.net/feeds/main",
+        "https://wongmjane.com/api/feed/rss",
+        "https://fourweekmba.com/feed",
+    ]
+
+    # Unite all feeds into one
+    all_feeds = substack_feeds + custom_feeds
+    return all_feeds
--- thinkpiecer/thinkpiecer.py
+++ thinkpiecer/thinkpiecer.py
@@ -9,7 +9,9 @@ import calendar
 from datetime import datetime
 import os, os.path
 
-import utilities
+# Local modules
+from utilities import safe_get
+import feeds
 
 # Constants
 WHOOSH_INDEX_DIR = "../whoosh_index3"
@@ -34,14 +36,15 @@ def build_new_index():
     if not os.path.exists(WHOOSH_INDEX_DIR):
         os.mkdir(WHOOSH_INDEX_DIR)
 
-    # Start the index
+    # Create the index
     ix = index.create_in(WHOOSH_INDEX_DIR, schema)
-
     return ix
 
 
 # Loads the search engine index from disk and returns it.
-# Only works if you already called `build_new_index()` before.
+# If no index exists, creates a brand-new one.
+# (if you have an index you want to overwrite,
+# call build_new_index again)
 def load_index():
     # Check if an index exists. If not, make one.
     # Obviously, has no effect if an index exists
@@ -50,9 +53,68 @@ def load_index():
 
     # Now return what we have
     ix = index.open_dir(WHOOSH_INDEX_DIR)
+    return ix
+
+
+# Given a list of RSS feeds and an index
+# (either new or existing), scrapes the list of
+# entries from the RSS feeds and adds them to the index
+# so we can search them.
+def add_articles_to_index(feed_list, ix):
+    # Create a writer to start adding entries
+    writer = ix.writer()
+
+    for feed_url in feed_list:
+        print(feed_url)
+        news_feed = feedparser.parse(feed_url)
+
+        # NOTE: we can only get the last few entries from this RSS feed.
+        # Substack doesn't seem to show anything older than the last 20.
+        # So we should build in a system to start caching these.
+
+        for entry in news_feed.entries:
+
+            # Get publication name. This is in the feed's `feed` field, along with other metadata
+            publication = None
+            metadata = safe_get(news_feed, 'feed')
+            if metadata is not None:
+                publication = safe_get(metadata, 'title')
+
+            # Clean up the date into a normal datetime
+            clean_datetime = datetime.fromtimestamp(calendar.timegm(entry['published_parsed']))
+
+            # Most feeds put the main content in `content`,
+            # but a rare few like Eugene Wei put it in `summary`
+            # (in which case `content` is empty). With this logic, let's get a single `content` field.
+            body_text = None
+            # See if `content` exists
+            content_holder = safe_get(entry, 'content')
+            if content_holder is not None:
+                # We have content; fill it in
+                content_tree = BeautifulSoup(content_holder[0]['value'], features="html.parser")
+                body_text = content_tree.get_text(" ", strip=True)
+            else:
+                # No content provided. `summary` must hold all the text.
+                summary_tree = BeautifulSoup(safe_get(entry, 'summary'), features="html.parser")
+                body_text = summary_tree.get_text(" ", strip=True)
+
+            writer.add_document(
+                title=safe_get(entry, 'title'),
+                author=safe_get(entry, 'author'),
+                publication=publication,
+                summary=safe_get(entry, 'summary'),
+                url=safe_get(entry, 'link'),
+                published=clean_datetime,
+                content=body_text)
+
+    print("DONE!")
+    writer.commit()
 
 
 def main():
-    print("Hi")
+    ix = load_index()
+    feed_list = feeds.get_feeds()
+    add_articles_to_index(feed_list, ix)
+
 
 if __name__ == '__main__': main()
