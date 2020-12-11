9103d5f2af6f24dde2aa2435f3512a04fdd3c880
Neel Mehta <neelmehta96@gmail.com>
2020-12-11 09:27:22 -0500

    i think?

--- app.py
+++ app.py
@@ -2,12 +2,27 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask.json import jsonify
+import os
 
 import thinkpiecer
 import utilities
 import feeds
 
+
 app = Flask(__name__)
+
+# def main():
+#     # Set up to run on the proper port
+#     port = int(os.environ.get('PORT', 33507))
+#     app.run(host='0.0.0.0', port=port)
+#
+# if __name__ == '__main__':
+#     main()
+
+port = int(os.environ.get('PORT', 33507))
+app.run(host='0.0.0.0', port=port)
+
+
 
 @app.route('/')
 def hello_world():
